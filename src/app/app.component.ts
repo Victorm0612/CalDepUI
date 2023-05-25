d389b567bd86f3497484ca4b11cc3f5115fffd4f
victorm0612 <victor.manuel.vargas@correounivalle.edu.co>
2023-05-24 23:35:12 -0500

    Add template and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import * as MiniZinc from 'minizinc';
-import { minizincWorker } from 'minizinc/minizinc-worker.js';
-import minizincWasm from 'minizinc/minizinc.wasm';
-import minizincData from 'minizinc/minizinc.data';
+import { switchMap, take } from 'rxjs';
+import { MinizincService } from './services';
 
 
 @Component({
@@ -12,7 +11,11 @@ import minizincData from 'minizinc/minizinc.data';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'cal-dep';
+  private readonly STATUS_DONE = 'OPTIMAL_SOLUTION';
+  model!: MiniZinc.Model;
+  showCalendar = false;
+  calendar: number[][] = [];
+  cost!: number;
   form = new FormGroup({
     n: new FormControl('', Validators.required),
     min: new FormControl('', Validators.required),
@@ -20,19 +23,49 @@ export class AppComponent implements OnInit {
     d: new FormControl('', Validators.required),
   });
 
+  constructor(
+    private service: MinizincService
+  ) { }
+
   ngOnInit() {
-    (async() => await MiniZinc.init({
-      workerURL: new URL('/assets/minizinc/dist/minizinc-worker.js', window.location.href),
-      wasmURL: new URL('/assets/minizinc/dist/minizinc.wasm', window.location.href),
-      dataURL: new URL('/assets/minizinc/dist/minizinc.data', window.location.href),
-    }).then(() => {
-      console.log('Ready');
-    }))();
-    const model = new MiniZinc.Model();
-    model.addFile('test.mzn', 'var 1..3: x;');
+    this.service.initializeService()
+      .pipe(
+        take(1),
+        switchMap(() => this.service.readModel())
+      )
+      .subscribe({
+        next: (model) => {
+          console.log('Ready');
+          this.model = new MiniZinc.Model();
+          this.model.addFile('CalDep.mzn', model);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    const { n, min, max, d } = this.form.value;
+    const dzn = `n=${n}; Min=${min}; Max=${max}; D=${d}`;
+    this.model.addDznString(dzn);
+    const solve = this.model.solve({
+      options: {
+        solver: 'gecode',
+        'all-solutions': true
+      }
+    });
+    solve.on('solution', solution => {
+      const output = solution.output.json;
+      if (output) {
+        this.cost = output['u'];
+        this.calendar = output['Cal'].reverse();
+      }
+    });
+    solve.then(result => {
+      if (result.status === this.STATUS_DONE) {
+        this.showCalendar = true;
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
