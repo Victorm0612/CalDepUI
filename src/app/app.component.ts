efdd1c0d66b8df98d69bcdfda749944997147230
Victorm0612 <victor.manuel.vargas@correounivalle.edu.co>
2023-05-24 20:06:19 -0500

    First desing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as MiniZinc from 'minizinc';
+import { minizincWorker } from 'minizinc/minizinc-worker.js';
+import minizincWasm from 'minizinc/minizinc.wasm';
+import minizincData from 'minizinc/minizinc.data';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cal-dep';
+  form = new FormGroup({
+    n: new FormControl('', Validators.required),
+    min: new FormControl('', Validators.required),
+    max: new FormControl('', Validators.required),
+    d: new FormControl('', Validators.required),
+  });
+
+  ngOnInit() {
+    (async() => await MiniZinc.init({
+      workerURL: new URL('/assets/minizinc/dist/minizinc-worker.js', window.location.href),
+      wasmURL: new URL('/assets/minizinc/dist/minizinc.wasm', window.location.href),
+      dataURL: new URL('/assets/minizinc/dist/minizinc.data', window.location.href),
+    }).then(() => {
+      console.log('Ready');
+    }))();
+    const model = new MiniZinc.Model();
+    model.addFile('test.mzn', 'var 1..3: x;');
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,3 @@
+declare module 'minizinc/minizinc-worker.js';
+declare module 'minizinc/minizinc.wasm';
+declare module 'minizinc/minizinc.data';
